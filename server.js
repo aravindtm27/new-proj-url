49ed9b6001a0be3f50d513fd02b1f1e9267f3407
aravindtm27 <145952967+aravindtm27@users.noreply.github.com>
2023-11-07 21:13:37 +0530

    auth

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 const ShortUrl = require('./models/shorturl');
 
+const userRoute = require("./route/user");
+
 mongoose.connect("mongodb://127.0.0.1:27017/urlshortner").then(()=>console.log("mongo connected"))
 
 app.listen(process.env.port || 5000, console.log("server has started"));
@@ -11,15 +13,22 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended:false}));
 
-app.get('/', async (req,res)=>{
-    const shortUrls = await ShortUrl.find();
-    res.render('index', {shortUrls:shortUrls});
-})
+app.use("/user", userRoute);
 
 app.post('/shortUrls', async (req,res)=>{
    await ShortUrl.create({full:req.body.fullUrl})
    res.redirect('/')
 })
+
+app.get('/', async (req,res)=>{
+    const shortUrls = await ShortUrl.find();
+    res.render('index', {shortUrls:shortUrls});
+});
+
+app.get('/signup', (req,res)=>{
+    res.render("signup");
+} )
+
 
 app.get('/:shortUrl',async (req,res)=>{
     const shortUrl = await ShortUrl.findOne({short:req.params.shortUrl})
